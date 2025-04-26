72df0f3e7521b8c9022d6a58bcd1c5f0ed1de743
unknown <YY526@ccs.local>
2025-04-26 17:52:30 +0800

    BACKEND 1TO5

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use('/accounts', require('./accounts/accounts.controller'));
 app.use('/api-docs', require('_helpers/swagger'));
 
 // global error handler
-app.use(errorHandler);
+app.use( errorHandler );
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
